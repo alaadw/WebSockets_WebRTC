090f1811169c859b09fb58c055cdf0eaae664401
Seyhun Sarıyıldız <seyhuns@gmail.com>
2012-04-15 16:16:09 +0300

    "socket.get" is removed and a global variable used if the socket data is reached from itself.

--- node_server/server.js
+++ node_server/server.js
@@ -1,7 +1,5 @@
 /**
- * Decleare HTTP server
- * Listen port 8080
- * Import socket.io
+ * node modules that are used.
  */
 var http = require('http'),
   server = http.createServer(),
@@ -9,10 +7,11 @@ var http = require('http'),
   io = require('socket.io').listen(app),
   check = require('validator').check,
   sanitize = require('validator').sanitize;
+
 /**
  * Sanitize the input string
- * @param  {[String]} inputString [The input from user]
- * @return {[String]}             [The normalized input]
+ * @param  {String} inputString : The input from user
+ * @return {String} : The sanitezed input
  */
 var filterInput = function (inputString){
   if (inputString){
@@ -23,50 +22,60 @@ var filterInput = function (inputString){
   return inputString;
 };
 
+/**
+ * Triggered when a user connects
+ */
 io.sockets.on('connection', function (socket) {
+
+  var user = {
+    username: 'guest' + socket.id,
+    room:     'lobby' // Default room
+  }
+
+  var updateRoomList = function (){
+    // Refresh the users in the chat room.
+    var usernames = [];
+    io.sockets.clients(user.room).forEach(function (socket){
+      socket.get('user', function (err, fetchedUser){
+        usernames.push(fetchedUser.username);
+      });
+    });
+    io.sockets.in(user.room).emit('updateUserList', usernames);
+  };
+
   /**
    * Add new user to the list. Assign user to the lobby.
-   * @param  {[string]} username [Desired username]
+   * @param  {string} username : Desired username
    */
   socket.on('addUser', function (username){
-    /**
-     * Prepare user variables
-     * @type {Object}
-     */
+
     username = filterInput(username);
-    var user = {
-      username: username ? username : 'guest' + socket.id,
-      room:'lobby'
-    };
+    if (username) {
+      user.username = username;
+    }
+
     /**
-     * Set user variable
+     * Set user variable to socket for reaching later.
      */
     socket.set('user', user, function (){
       // Add user to chat room
       socket.join(user.room);
-      // Refresh the users in the chat room.
-      var usernames = [];
-      io.sockets.clients(user.room).forEach(function (socket){
-        socket.get('user', function (err, fetchedUser){
-          usernames.push(fetchedUser.username);
-        });
-      });
-      /**
-       * Set user values in client side
-       */
+      updateRoomList();
       socket.emit('setClientData', user);
-      io.sockets.in(user.room).emit('updateUserList', usernames);
     });
   });
+
   /**
    * Broadcast message to the group
    * @param  {[type]} message
    */
   socket.on('sendMessage', function (message){
-    message = filterInput(message);
-    socket.get('user', function (err, user){
-      console.log(user.room, message);
-      io.sockets.in(user.room).emit('updateChat', '<b>' + user.username + ':</b> ' + message);
-    });
+    io.sockets.in(user.room).emit('updateChat', '<b>' + user.username + ':</b> ' + filterInput(message));
   });
+
+  socket.on('disconnect', function (){
+    socket.leave(user.room);
+    updateRoomList(user.room);
+  });
+
 });
